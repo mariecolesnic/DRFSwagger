4de6ac26efcb20b58f87762a106a78e80b750eaf
Marie Colesnic <melterina96@gmail.com>
2021-10-20 15:00:37 +0300

    Added models and admin pages to myapi

--- djangitos-master/myapi/admin.py
+++ djangitos-master/myapi/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(models.Category, CategoryAdmin)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(models.Author, AuthorAdmin)
+
+
+class BookAdmin(admin.ModelAdmin):
+    filter_horizontal = ('authors', )
+    list_display = ('title', 'category',)
+
+
+admin.site.register(models.Book, BookAdmin)
